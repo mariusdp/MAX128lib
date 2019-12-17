07b3ed92af9c4239192659eb7fb165eeb8c52428
Marius-Daniel Pozna <MPozna@rockwellautomation.com>
2019-12-17 16:33:55 +0000

    Version 1.0 - Library in the lib folder

--- src/main.cpp
+++ src/main.cpp
@@ -13,9 +13,9 @@
    
 //byte address = 0x28;
 int address = 0x28;
-int data = 0;
-int channel = 2;
-float volt;
+//int data = 0;
+//int channel = 2;
+//float volt;
 
 MAX128 adc(address);
 
@@ -25,11 +25,25 @@ void setup(){
 
 }
 void loop(){
- data = adc.readADC(channel);
- volt = (5.096/4096)*data; //External ref conected on 5V out ESP32 (measured with DMM)
- Serial.print("Data: ");
- Serial.println(data);
- Serial.print("Volt: ");
- Serial.println(volt);
- delay(1000);
+ int data0 = adc.readADC(0);
+ int data1 = adc.readADC(1);
+ int data2 = adc.readADC(2);
+ float volt0 = (5.096/4096)*data0; //External ref conected on 5V out ESP32 (measured with DMM)
+ float volt1 = (5.096/4096)*data1; //External ref conected on 5V out ESP32 (measured with DMM)
+ float volt2 = (24.500/4096)*data2; //External ref conected on 5V out ESP32 (measured with DMM)
+ Serial.println("------------------------------");
+ Serial.print("Data 0: ");
+ Serial.println(data0);
+ Serial.println("Volt 0: " + String(volt0) +" V");
+ //Serial.println(volt0);
+ Serial.print("Data 1: ");
+ Serial.println(data1);
+ Serial.println("Volt 1: " + String(volt1) +" V");
+ //Serial.println(volt1);
+ Serial.print("Data 2: ");
+ Serial.println(data2);
+ Serial.println("Volt 2: " + String(volt2) +" V");
+// Serial.println(volt2);
+ Serial.println("------------------------------");
+ delay(2000);
 }
